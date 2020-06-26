4fa8a56126b460f62ec885cd05b893953c5da20c
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-06-26 21:50:57 +0800

    more comments

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jamesxuhaozhe/tianchimiddlewarecompetition/constants"
 )
 
+// IsClientProcess checks if the current process is client.
 func IsClientProcess() bool {
 	serverPort := conf.GetServerPort()
 	if serverPort == constants.ClientProcessPort1 || serverPort == constants.ClientProcessPort2 {
@@ -15,6 +16,7 @@ func IsClientProcess() bool {
 	return false
 }
 
+// IsBackendProcess checks if the current process is backend.
 func IsBackendProcess() bool {
 	serverPort := conf.GetServerPort()
 	if serverPort == constants.BackendProcessPort1 {
@@ -23,6 +25,7 @@ func IsBackendProcess() bool {
 	return false
 }
 
+// MD5 returns the md5 hash of the input string.
 func MD5(s string) string {
 	return md5Bytes([]byte(s))
 }
